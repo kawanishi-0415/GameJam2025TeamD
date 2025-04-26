cede2920e9e9346826e032993c0424a1a195d5d2
kawanishi <kazu.w.0.custom@gmail.com>
2025-04-26 21:04:27 +0900

    クリア時のGameScene切り替え

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -140,7 +140,8 @@ public class StageManager : MonoBehaviour
 
         if (CheckAllClear())
         {
-            ReloadCurrentScene();
+            GameSceneManager gameSceneManager = FindObjectOfType<GameSceneManager>();
+            gameSceneManager.ChangeScene(GameSceneManager.GameState.RESULT);
         }
         else
         {
