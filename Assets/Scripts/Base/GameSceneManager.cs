12ab773f69aac3a31962b8499ced41346086d13a
RauNyan0 <kirisimanozomi@gmail.com>
2025-04-26 23:11:36 +0900

    シーン名修正

--- Assets/Scripts/Base/GameSceneManager.cs
+++ Assets/Scripts/Base/GameSceneManager.cs
@@ -14,7 +14,7 @@ public class GameSceneManager : MonoBehaviour
         RESULT,
     }
     private GameState m_State = GameState.TITLE;
-    private string[] m_SceneName = { "Title", "Stage", "Game" };
+    private string[] m_SceneName = { "Title", "Stage", "Result" };
     private bool m_IsFadeing = false;
 
     private void Awake()
